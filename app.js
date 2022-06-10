00ddc6304d60f06b0a34c470061f38fb24bb3e38
arimtiaz <abadur391@gmail.com>
2022-06-10 17:00:50 +0600

    started

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('I am in a separate file')
+
+document.getElementById("add-case").addEventListener("click", function(){
+    const caseInput = document.getElementById("case-count");
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 5;
+})

9e2db45b0b5f484540c16ce0edb25592dd84edd2
arimtiaz <abadur391@gmail.com>
2022-06-11 20:35:47 +0600

    3rd part

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 
 function updateCaseNumber(isIncreasing){
     const caseInput = document.getElementById("case-count");
-    const caseNumber = caseInput.value;
+    let caseNumber = caseInput.value;
     if(isIncreasing == true){
-        caseInput.value = parseInt(caseNumber)+1;
-    } else {
-        caseInput.value = parseInt(caseNumber)-1;
+        caseNumber = parseInt(caseNumber)+1;
+    } else if(caseNumber > 0) {
+        caseNumber= parseInt(caseNumber)-1;
     }
+    caseInput.value = caseNumber;
+    // updating price with quantity
+    const caseQuantity = document.getElementById("case-quantity");
+    caseQuantity.innerText= caseNumber * 59;
+
 }
+
+
 
 document.getElementById("add-case").addEventListener("click", function(){
     updateCaseNumber(true);
